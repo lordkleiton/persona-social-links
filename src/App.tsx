b315e005c9f61ccb5fc8750875fae1ee2d67b525
lord <m_s_r@outlook.com>
2021-09-13 23:29:24 -0300

    adicionando assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import Card from "./components/Card";
-import p3data from "./data/p3";
+import Main from "./routes/Main";
 
 function App() {
-  const url =
-    "https://sites.google.com/site/persona3fessociallink/_/rsrc/1398700674440/about-me/aaaaaaaaaaaaaa.jpg?height=200&width=182";
-
-  const p3 = Object.values(p3data);
-
-  return (
-    <>
-      {p3.map((link) => (
-        <Card
-          imagePath={url}
-          name={link.name}
-          arcana={link.arcana}
-          key={`${link.arcana}-${link.name}`}
-        />
-      ))}
-    </>
-  );
+  return <Main />;
 }
 
 export default App;
---
+++ src/routes/Main/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "../../components/Card";
+import p3data from "../../data/p3";
+import { Background } from "./styles";
+
+const Main: React.FC = () => {
+  const url =
+    "https://sites.google.com/site/persona3fessociallink/_/rsrc/1398700674440/about-me/aaaaaaaaaaaaaa.jpg?height=200&width=182";
+
+  const p3 = Object.values(p3data);
+
+  return (
+    <Background>
+      {p3.map((link) => (
+        <Card
+          imagePath={url}
+          name={link.name}
+          arcana={link.arcana}
+          key={`${link.arcana}-${link.name}`}
+        />
+      ))}
+    </Background>
+  );
+};
+
+export default Main;
