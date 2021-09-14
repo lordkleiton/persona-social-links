dfe54dc4115e42c5d9e42ffa293a9b886dbc43be
lord <m_s_r@outlook.com>
2021-09-13 22:28:15 -0300

    exibindo os adicionados anteriormente na tela

--- src/data/p3/index.ts
+++ src/data/p3/index.ts
@@ -3,21 +3,31 @@ import chihiro from "./chihiro";
 import fuuka from "./fuuka";
 import hidetoshi from "./hidetoshi";
 import kazuchi from "./kazushi";
+import keisuke from "./keisuke";
 import kenji from "./kenji";
+import maiko from "./maiko";
+import maya from "./maya";
 import mitsuru from "./mitsuru";
+import pharos from "./pharos";
 import SEES from "./sees";
 import yukari from "./yukari";
+import yuko from "./yuko";
 
 const exportData = {
-  SEES,
-  kenji,
-  fuuka,
-  mitsuru,
-  hidetoshi,
   bunkichi_mitsuko,
-  yukari,
-  kazuchi,
   chihiro,
+  fuuka,
+  hidetoshi,
+  kazuchi,
+  keisuke,
+  kenji,
+  maiko,
+  maya,
+  mitsuru,
+  pharos,
+  SEES,
+  yukari,
+  yuko,
 };
 
 export default exportData;
