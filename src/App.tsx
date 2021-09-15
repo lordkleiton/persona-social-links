d026846a852eba51075e2c6544babbd1b9db1e66
Mario Silva <mario@mobills.com.br>
2021-09-15 11:40:46 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import Main from "./routes/Main";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import GameRoute from "./routes/GameRoute";
 
 function App() {
-  return <Main />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/:game">
+          <GameRoute />
+        </Route>
+
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
